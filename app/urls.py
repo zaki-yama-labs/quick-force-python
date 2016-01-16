aa3412124d49154210a2cc86aec2a079245493fb
zaki-yama <shingoyamazaki00@gmail.com>
2016-01-17 01:58:05 +0900

    redirect to setup if not setup

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 
 from .views import IndexView, SetupView
 
+app_name = 'app'
 urlpatterns = [
-    url('^setup/?$', SetupView.as_view()),
+    url('^setup/?$', SetupView.as_view(), name='setup'),
     url('^$', IndexView.as_view()),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
+import os
+
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import View
+
+
+def is_setup():
+    return (os.environ.get('CONSUMER_KEY') is not None
+            and os.environ.get('CONSUMER_SECRET') is not None)
 
 
 class IndexView(View):
     template_name = 'app/index.html'
 
     def get(self, request, *args, **kwargs):
+        if not is_setup():
+            return redirect(reverse('app:setup'))
+
         return render(request, self.template_name)
 
 
