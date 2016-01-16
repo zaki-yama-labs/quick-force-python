c72a4df4a8e967bbf555cd519930bf455a63e073
zaki-yama <shingoyamazaki00@gmail.com>
2016-01-17 01:55:17 +0900

    fix INSTALLED_APPS (this is 1.9 style)

--- quick_force_python/settings.py
+++ quick_force_python/settings.py
@@ -28,13 +28,13 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
 )
 
 MIDDLEWARE_CLASSES = (
