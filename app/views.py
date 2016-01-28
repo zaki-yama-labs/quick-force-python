da8f2c5f63ce729f32165f3bbef3921b8ff53240
zaki-yama <shingoyamazaki00@gmail.com>
2016-01-28 15:22:04 +0900

    implement auth and redirect logic

--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 import os
 
+from logging import getLogger, StreamHandler, DEBUG
+
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import View
+
+from .auth import SalesforceOAuth2
+
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+
+REDIRECT_URI = 'http://localhost:5000'
 
 
 def is_setup():
@@ -18,6 +30,20 @@ class IndexView(View):
     def get(self, request, *args, **kwargs):
         if not is_setup():
             return redirect(reverse('app:setup'))
+
+        connection = SalesforceOAuth2(
+                client_id=os.environ.get('CONSUMER_KEY'),
+                client_secret=os.environ.get('CONSUMER_SECRET'),
+                redirect_uri=REDIRECT_URI)
+
+        if 'code' in request.GET:
+            res = connection.get_token(request.GET['code'])
+            for k, v in res.items():
+                logger.debug('%s: %r' % (k, v))
+        else:
+            auth_url = connection.authorize_url(scope='full')
+            logger.debug('redirect to: {}'.format(auth_url))
+            return redirect(auth_url)
 
         return render(request, self.template_name)
 
